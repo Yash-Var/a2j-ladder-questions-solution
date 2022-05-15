0990863348019e5c88f46b52e6148af557feef13
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-05-15 18:25:37 +0530

    some change

--- Codeforces Div. 2, A/08-Lucky-Division.cpp
+++ Codeforces Div. 2, A/08-Lucky-Division.cpp
@@ -4,15 +4,15 @@ int main()
 {
     int n;
     cin >> n;
-    int arr[] = {4, 7, 44, 77, 47, 447, 474, 444, 777, 747, 744, 774, 477};
     //     int ram=arr.size()-1;
+    int ar[] = {4, 7, 44, 77, 47, 447, 474, 444, 777, 747, 744, 774, 477};
     bool isbool = false;
     for (int i = 0; i < 13; i++)
     {
-        if (n % arr[i] == 0)
         {
+        if (n % ar[i] == 0)
             isbool = true;
-            //     cout<<arr[i]<<endl;
+            
         }
     }
     if (isbool)
