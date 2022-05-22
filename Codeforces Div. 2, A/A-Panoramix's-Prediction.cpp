0ff14cf915de2674c95a81045bbce1c0e018d89a
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-05-22 21:02:01 +0530

    add some important question

--- Codeforces Div. 2, A/A-Panoramix's-Prediction.cpp
+++ Codeforces Div. 2, A/A-Panoramix's-Prediction.cpp
@@ -3,30 +3,54 @@
 using namespace std;
 bool checkPrime(int n)
 {
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
+        int flag=0;
+        int m=n/2;  
+    for( int i = 2; i <=m ; i++)  
+  {  
+      if(n % i == 0)  
+      {  
+        //   cout<<"Number is not Prime."<<endl;  
+          flag=1;  
+          break;  
+      }  
+  }  
+  if (flag==0)  {
+        //   cout<<"nvflkgkr"<<endl;
+          return true;
+  }
+//       cout << "Number is Prime."<<endl; 
+
+return false;
 }
 int main()
 {
     int n, m;
+    int flag=0;
     cin >> n >> m;
     if (checkPrime(n))
     {
-        while (1)
+           
+        while (n<=m)
         {
             n++;
+        //      cout<<"yes"<<endl;
             if (checkPrime(n))
             {
                 if (n == m)
                 {
-                    cout << "Yes" << endl;
+                //     cout << "Yes" << endl;
+                flag=1;
+                  
                 }
-                else
-                    cout << "No" << endl;
+          break;
+                //     cout << "No" << endl;
             }
         }
     }
+    if(flag==1){
+            cout<<"Yes"<<endl;
+    }else cout<<"No"<<endl;
+    
 
     return 0;
 }
